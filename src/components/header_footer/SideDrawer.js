47751cd3db43269284ea3815c939f5ab2c12a76e
Renish B <renish.bhaskaran@globalenglish.com>
2019-01-20 00:38:22 +0530

    added basic navbar and sidebar

---
+++ src/components/header_footer/SideDrawer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const SideDrawer = (props) => {
+  return (
+    <Drawer
+        anchor="right"
+        open={props.open}
+        onClose={() => props.onClose(false)}
+    >
+    <List component="nav">
+        <ListItem button onClick={() => console.log('Event Starts')}>
+          Event Starts in
+        </ListItem>
+
+        <ListItem button onClick={() => console.log('Venue Info')}>
+          Venue Info
+        </ListItem>
+
+        <ListItem button onClick={() => console.log('Highlights')}>
+          Highlights
+        </ListItem>
+
+        <ListItem button onClick={() => console.log('Pricing')}>
+          Pricing
+        </ListItem>
+
+        <ListItem button onClick={() => console.log('Locations')}>
+          Locations
+        </ListItem>
+    </List>
+    </Drawer>
+  )
+}
+
+export default SideDrawer;
