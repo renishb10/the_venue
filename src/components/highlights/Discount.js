e48670a1fbd364d578c32f2791d3837841212f6c
Renish B <renish.bhaskaran@globalenglish.com>
2019-01-20 16:53:18 +0530

    fixed location div and spacing issue

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -27,27 +27,37 @@ class Discount extends Component {
 
   render() {
     return (
-      <div className="center_wrapper">
-        
-        <div className="discount_wrapper">
-            <Fade
-                onReveal={() => this.percentage()}
-            >
-                <div className="discount_percentage">
-                    <span>{this.state.discountStart}%</span>
-                    <span>OFF</span>
-                </div>
-            </Fade>
+        <div className="center_wrapper">
+                <div className="discount_wrapper">
 
-            <Slide right>
-                <div className="discount_description">
-                    <h3>Purchase tickets before 15th December</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                    <CustomButton text="Purchase Tickets" bck="#ffa800" color="#ffffff" link="#" />
+                    <Fade
+                        onReveal={()=> this.percentage()}
+                    >
+                        <div className="discount_percentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+                    
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase tickets before 20th JUNE</h3>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                        
+                            <CustomButton
+                                text="Purchase tickets"
+                                bck="#ffa800"
+                                color="#ffffff"
+                                link="http://google.com"
+                            />
+                        
+                        </div>
+                    </Slide>
+                    
+
+
                 </div>
-            </Slide> 
-        </div>
-      </div>
+            </div>
     )
   }
 }
